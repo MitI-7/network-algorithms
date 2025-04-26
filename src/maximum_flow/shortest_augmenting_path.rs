7b7a6e370ad5339d2b257903bec6d7777420c7b4
MitI_7 <miti_7@outlook.com>
2025-04-26 11:27:14 +0900

    fix

---
+++ src/maximum_flow/shortest_augmenting_path.rs
@@ -0,0 +1,62 @@
+use crate::maximum_flow::csr::CSR;
+use crate::maximum_flow::graph::Graph;
+use crate::maximum_flow::status::Status;
+use num_traits::NumAssign;
+
+#[derive(Default)]
+pub struct ShortestAugmentingPath<Flow> {
+    csr: CSR<Flow>,
+    pub current_edge: Vec<usize>,
+}
+
+impl<Flow> ShortestAugmentingPath<Flow>
+where
+    Flow: NumAssign + Ord + Copy,
+{
+    pub fn solve(&mut self, graph: &mut Graph<Flow>, source: usize, sink: usize, upper: Option<Flow>) -> Result<Flow, Status> {
+        self.csr.build(graph);
+        self.csr.update_distances_to_sink(source, sink);
+        self.current_edge.resize(self.csr.num_nodes, 0);
+
+        let mut flow = Flow::zero();
+        let upper = upper.unwrap_or_else(|| self.csr.neighbors(source).fold(Flow::zero(), |sum, i| sum + self.csr.upper[i]));
+        while self.csr.distances_to_sink[source] < self.csr.num_nodes {
+            self.current_edge.iter_mut().enumerate().for_each(|(u, e)| *e = self.csr.start[u]);
+            if let Some(delta) = self.dfs(source, sink, upper) {
+                flow += delta;
+            }
+        }
+
+        self.csr.set_flow(graph);
+        Ok(flow)
+    }
+
+    fn dfs(&mut self, u: usize, sink: usize, upper: Flow) -> Option<Flow> {
+        if u == sink {
+            return Some(upper);
+        }
+
+        for i in self.current_edge[u]..self.csr.start[u + 1] {
+            self.current_edge[u] = i;
+            let to = self.csr.to[i];
+            if self.csr.is_admissible_edge(u, i) {
+                // advance
+                if let Some(delta) = self.dfs(to, sink, upper.min(self.csr.residual_capacity(i))) {
+                    self.csr.push_flow(i, delta);
+                    return Some(delta);
+                }
+            }
+        }
+
+        // retreat
+        self.csr.distances_to_sink[u] = self.csr.num_nodes;
+        for i in self.csr.neighbors(u) {
+            let to = self.csr.to[i];
+            if self.csr.residual_capacity(i) > Flow::zero() {
+                self.csr.distances_to_sink[u] = self.csr.distances_to_sink[u].min(self.csr.distances_to_sink[to] + 1);
+            }
+        }
+
+        None
+    }
+}
