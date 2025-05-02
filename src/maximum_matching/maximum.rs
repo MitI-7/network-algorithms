0a3d1890a98757172f9e030ef7fb64a2c28670ad
MitI_7 <miti_7@outlook.com>
2025-05-02 22:05:51 +0900

    fix

--- src/maximum_matching/maximum.rs
+++ src/maximum_matching/maximum.rs
@@ -4,16 +4,16 @@ use std::collections::VecDeque;
 #[derive(Copy, Clone, Debug, PartialEq)]
 enum NodeType {
     Unvisited,
-    Even,
-    Odd,
+    Even, // the number of edges in the unique path from the root node to node i in the alternating tree is even
+    Odd,  // the number of edges in the unique path from the root node to node i in the alternating tree is odd
 }
 
 #[derive(Default)]
 pub struct Blossom {
     mate: Box<[Option<usize>]>,
     parent: Box<[usize]>,
-    label: Box<[usize]>,
-    node_type: Box<[NodeType]>,
+    component: Box<[usize]>,
+    labels: Box<[NodeType]>,
     even_nodes: VecDeque<usize>,
     in_queue: Box<[bool]>,
     time_stamp: Box<[usize]>,
@@ -51,8 +51,8 @@ impl Blossom {
 
         self.mate = vec![None; num_nodes].into_boxed_slice();
         self.parent = vec![usize::MAX; num_nodes].into_boxed_slice();
-        self.label = (0..num_nodes).collect();
-        self.node_type = vec![NodeType::Unvisited; num_nodes].into_boxed_slice();
+        self.component = (0..num_nodes).collect();
+        self.labels = vec![NodeType::Unvisited; num_nodes].into_boxed_slice();
         self.in_queue = vec![false; num_nodes].into_boxed_slice();
         self.time_stamp = vec![0; num_nodes].into_boxed_slice();
 
@@ -81,27 +81,27 @@ impl Blossom {
     }
 
     fn find_augmenting_path(&mut self, root: usize) -> Option<usize> {
-        self.label.iter_mut().enumerate().for_each(|(i, x)| *x = i);
-        self.node_type.fill(NodeType::Unvisited);
+        self.component.iter_mut().enumerate().for_each(|(i, x)| *x = i);
+        self.labels.fill(NodeType::Unvisited);
         self.in_queue.fill(false);
         self.even_nodes.clear();
 
         self.even_nodes.push_back(root);
-        self.node_type[root] = NodeType::Even;
+        self.labels[root] = NodeType::Even;
         self.in_queue[root] = true;
         while let Some(u) = self.even_nodes.pop_front() {
-            assert_eq!(self.node_type[u], NodeType::Even);
+            assert_eq!(self.labels[u], NodeType::Even);
 
             for i in self.neighbors(u) {
                 let v = self.to[i];
-                match self.node_type[v] {
+                match self.labels[v] {
                     NodeType::Unvisited => {
                         self.parent[v] = u;
-                        self.node_type[v] = NodeType::Odd;
+                        self.labels[v] = NodeType::Odd;
 
                         match self.mate[v] {
                             Some(w) => {
-                                self.node_type[w] = NodeType::Even;
+                                self.labels[w] = NodeType::Even;
                                 self.parent[w] = v; // u -> v -> w
                                 if !self.in_queue[w] {
                                     self.even_nodes.push_back(w);
@@ -109,15 +109,15 @@ impl Blossom {
                                 }
                             }
                             None => {
-                                // find an augmenting path
+                                // whenever an unmatched node has an odd label, the path joining the root node to this node is an augmenting path
                                 return Some(v);
                             }
                         }
                     }
                     NodeType::Even => {
                         // find blossom
-                        if self.label[u] != self.label[v] {
-                            let base = self.get_base(u, v);
+                        if self.component[u] != self.component[v] {
+                            let base = self.find_base_of_blossom(u, v);
                             self.contract(u, v, base);
                             self.contract(v, u, base);
                         }
@@ -144,7 +144,7 @@ impl Blossom {
         }
     }
 
-    fn get_base(&mut self, mut u: usize, mut v: usize) -> usize {
+    fn find_base_of_blossom(&mut self, mut u: usize, mut v: usize) -> usize {
         self.time += 1;
         loop {
             if u != usize::MAX {
@@ -155,28 +155,31 @@ impl Blossom {
                 assert_ne!(self.time_stamp[u], self.time);
                 self.time_stamp[u] = self.time;
 
-                u = if let Some(x) = self.mate[u] { self.label[self.parent[x]] } else { usize::MAX }
+                u = if let Some(x) = self.mate[u] { self.component[self.parent[x]] } else { usize::MAX }
             }
             std::mem::swap(&mut u, &mut v);
         }
     }
 
-    fn contract(&mut self, entry: usize, mut bridge_even: usize, base: usize) {
+    fn contract(&mut self, entry: usize, mut peer: usize, base: usize) {
+        assert_eq!(self.labels[entry], self.labels[peer]);
+
         let mut now = entry;
-        while self.label[now] != base {
-            self.parent[now] = bridge_even;
+        while self.component[now] != base {
+            self.parent[now] = peer;
+            peer = self.mate[now].unwrap();
 
-            bridge_even = self.mate[now].unwrap();
-            if self.node_type[bridge_even] == NodeType::Odd {
-                self.node_type[bridge_even] = NodeType::Even;
-                assert!(!self.in_queue[bridge_even]);
-                self.even_nodes.push_back(bridge_even);
-                self.in_queue[bridge_even] = true;
+            self.component[now] = base;
+            self.component[peer] = base;
+
+            if self.labels[peer] == NodeType::Odd {
+                self.labels[peer] = NodeType::Even;
+                assert!(!self.in_queue[peer]);
+                self.even_nodes.push_back(peer);
+                self.in_queue[peer] = true;
             }
-            self.label[now] = base;
-            self.label[bridge_even] = base;
 
-            now = self.parent[bridge_even];
+            now = self.parent[peer];
         }
     }
 
