1361ba7ca85057fef581c344cd51db926527f48a
MitI_7 <miti_7@outlook.com>
2025-05-04 10:43:15 +0900

    fix

--- examples/maximum_bipartite_matching.rs
+++ examples/maximum_bipartite_matching.rs
@@ -1,9 +1,7 @@
-use network_algorithms::maximum_bipartite_matching::hopcroft_karp::HopcroftKarp;
-use std::collections::VecDeque;
+use network_algorithms::maximum_bipartite_matching::{BipartiteGraph, HopcroftKarp};
 
 use std::io::*;
 use std::str::FromStr;
-use std::vec;
 
 fn read<T: FromStr>() -> T {
     let stdin = stdin();
@@ -17,14 +15,22 @@ fn read<T: FromStr>() -> T {
     token.parse().ok().expect("failed to parse token")
 }
 
+// https://judge.yosupo.jp/problem/bipartitematching
 fn main() {
-    // let (num_left_nodes, num_right_nodes, num_edges) = (read(), read(), read());
-    // let mut hk = HopcroftKarp::new(num_left_nodes, num_right_nodes);
-    //
-    // for _ in 0..num_edges {
-    //     let (a, b) = (read(), read());
-    //     hk.add_edge(a, b);
-    // }
-    //
-    // println!("{}", hk.solve().len());
+    let (num_left_nodes, num_right_nodes, num_edges): (usize, usize, usize) = (read(), read(), read());
+
+    let mut graph = BipartiteGraph::default();
+    let left_nodes = graph.add_left_nodes(num_left_nodes);
+    let right_nodes = graph.add_right_nodes(num_right_nodes);
+    for _ in 0..num_edges {
+        let (a, b): (usize, usize) = (read(), read());
+        graph.add_undirected_edge(left_nodes[a], right_nodes[b]);
+    }
+    let matching = HopcroftKarp::default().solve(&graph);
+
+    println!("{}", matching.len());
+    for &edge_id in matching.iter() {
+        let edge = &graph.get_edge(edge_id).unwrap();
+        println!("{} {}", edge.u, edge.v);
+    }
 }
--- src/maximum_bipartite_matching/mod.rs
+++ src/maximum_bipartite_matching/mod.rs
@@ -1,2 +1,5 @@
 pub mod bipartite_graph;
 pub mod hopcroft_karp;
+
+pub use self::bipartite_graph::BipartiteGraph;
+pub use self::hopcroft_karp::HopcroftKarp;
