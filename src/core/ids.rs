eb1ebc1cfd8f3d7d56b2a11faa731d450329a551
MitI_7 <miti_7@outlook.com>
2025-05-26 12:36:07 +0900

    fix

--- src/core/ids.rs
+++ src/core/ids.rs
@@ -1,16 +1,26 @@
-#[derive(Copy, Clone, Debug, PartialEq, Eq, Hash)]
+#[derive(Copy, Clone, Debug, PartialEq, Eq, Hash, Ord, PartialOrd)]
 pub struct NodeId(pub usize);
 
-impl From<NodeId> for usize { #[inline] fn from(n: NodeId) -> Self { n.0 } }
-
-impl NodeId {
-    #[inline] pub fn index(self) -> usize { self.0 }
+impl From<NodeId> for usize {
+    #[inline]
+    fn from(n: NodeId) -> Self {
+        n.0
+    }
 }
 
-#[derive(Copy, Clone, Debug, PartialEq, Eq, Hash)]
+impl NodeId {
+    #[inline]
+    pub fn index(self) -> usize {
+        self.0
+    }
+}
+
+#[derive(Copy, Clone, Debug, PartialEq, Eq, Hash, Ord, PartialOrd)]
 pub struct EdgeId(pub usize);
 
 impl EdgeId {
-    #[inline] pub fn index(self) -> usize { self.0 }
+    #[inline]
+    pub fn index(self) -> usize {
+        self.0
+    }
 }
-
