12a655425f380628ce59ae1773ed1a47ecf7c44b
MitI_7 <miti_7@outlook.com>
2025-05-26 12:54:37 +0900

    再帰関数

--- src/algorithms/spanning_tree/edmonds.rs
+++ src/algorithms/spanning_tree/edmonds.rs
@@ -3,6 +3,7 @@ use crate::core::graph::Graph;
 use crate::edge::weight::WeightEdge;
 use crate::prelude::EdgeId;
 use crate::traits::{IntNum, Zero};
+use std::marker::PhantomData;
 
 #[derive(Clone)]
 struct Edge<W> {
@@ -14,87 +15,90 @@ struct Edge<W> {
 
 // O(nm)
 #[derive(Default)]
-pub struct Edmonds {}
+pub struct Edmonds<W> {
+    phantom_data: PhantomData<W>,
+}
 
-impl Edmonds {
-    pub fn solve<W: IntNum + Zero>(&self, graph: &Graph<Directed, (), WeightEdge<W>>, original_root: usize) -> Option<(W, Vec<EdgeId>)> {
-        let inf = W::max_value();
-        
-        let mut num_nodes = graph.num_nodes();
-        let mut root = original_root;
+impl<W> Edmonds<W>
+where
+    W: IntNum + Zero,
+{
+    pub fn solve(&self, graph: &Graph<Directed, (), WeightEdge<W>>, root: usize) -> Option<(W, Vec<EdgeId>)> {
         let mut edges = Vec::with_capacity(graph.num_edges());
         for (i, edge) in graph.edges.iter().enumerate() {
             edges.push(Edge { id: EdgeId(i), from: edge.u.index(), to: edge.v.index(), cost: edge.data.weight });
         }
 
+        self.minimum_cost(graph.num_nodes(), &edges, root)
+    }
+
+    fn minimum_cost(&self, num_nodes: usize, edges: &Vec<Edge<W>>, root: usize) -> Option<(W, Vec<EdgeId>)> {
+        let inf = W::max_value();
         let mut total_cost = W::zero();
-        loop {
-            let mut in_cost = vec![inf; num_nodes];
-            let mut parent = vec![usize::MAX; num_nodes];
-            let mut e = vec![EdgeId(usize::MAX); num_nodes];
-            for &Edge { id, from, to, cost } in edges.iter() {
-                if from != to && cost < in_cost[to] {
-                    in_cost[to] = cost;
-                    parent[to] = from;
-                    e[to] = id;
-                }
-            }
-            in_cost[root] = W::zero();
-            for &c in in_cost.iter() {
-                if c == inf {
-                    return None;
-                }
-                total_cost += c;
-            }
-            
-            // decomposition of strongly connected components
-            let mut ids = vec![usize::MAX; num_nodes];
-            let mut scc_cnt = 0;
-            {
-                let mut stamp = vec![usize::MAX; num_nodes];
-                for u in 0..num_nodes {
-                    let mut v = u;
-                    while stamp[v] != u && ids[v] == usize::MAX && v != root {
-                        stamp[v] = u;
-                        v = parent[v];
-                    }
-                    // find cycle
-                    if v != root && ids[v] == usize::MAX {
-                        let mut w = parent[v];
-                        ids[v] = scc_cnt;
-                        while w != v {
-                            ids[w] = scc_cnt;
-                            w = parent[w];
-                        }
-                        scc_cnt += 1;
-                    }
-                }
 
-                // no cycle
-                if scc_cnt == 0 {
-                    break;
-                }
-
-                for u in 0..num_nodes {
-                    if ids[u] == usize::MAX {
-                        ids[u] = scc_cnt;
-                        scc_cnt += 1;
-                    }
-                }
+        let mut in_cost = vec![inf; num_nodes];
+        let mut parent = vec![usize::MAX; num_nodes];
+        for &Edge { id, from, to, cost } in edges.iter() {
+            if from != to && cost < in_cost[to] {
+                in_cost[to] = cost;
+                parent[to] = from;
             }
-
-            // contraction
-            let mut next_edges = Vec::with_capacity(edges.len());
-            for &Edge { id, from, to, cost } in edges.iter() {
-                if ids[from] != ids[to] {
-                    next_edges.push(Edge { id, from: ids[from], to: ids[to], cost: cost - in_cost[to] });
-                }
-            }
-            root = ids[root];
-            num_nodes = scc_cnt;
-            std::mem::swap(&mut edges, &mut next_edges);
         }
-        
-        Some((total_cost, Vec::new()))
+        in_cost[root] = W::zero();
+        for &c in in_cost.iter() {
+            if c == inf {
+                return None;
+            }
+            total_cost += c;
+        }
+
+        // decomposition of strongly connected components
+        let mut ids = vec![usize::MAX; num_nodes];
+        let mut scc_cnt = 0;
+        {
+            let mut stamp = vec![usize::MAX; num_nodes];
+            for u in 0..num_nodes {
+                let mut v = u;
+                while stamp[v] != u && ids[v] == usize::MAX && v != root {
+                    stamp[v] = u;
+                    v = parent[v];
+                }
+                // find cycle
+                if v != root && ids[v] == usize::MAX {
+                    let mut w = parent[v];
+                    ids[v] = scc_cnt;
+                    while w != v {
+                        ids[w] = scc_cnt;
+                        w = parent[w];
+                    }
+                    scc_cnt += 1;
+                }
+            }
+
+            // no cycle
+            if scc_cnt == 0 {
+                return Some((total_cost, Vec::new()));
+            }
+
+            for u in 0..num_nodes {
+                if ids[u] == usize::MAX {
+                    ids[u] = scc_cnt;
+                    scc_cnt += 1;
+                }
+            }
+        }
+
+        // contraction
+        let mut next_edges = Vec::with_capacity(edges.len());
+        for &Edge { id, from, to, cost } in edges.iter() {
+            if ids[from] != ids[to] {
+                next_edges.push(Edge { id, from: ids[from], to: ids[to], cost: cost - in_cost[to] });
+            }
+        }
+
+        match self.minimum_cost(scc_cnt, &next_edges, ids[root]) {
+            Some((c, e)) => Some((c + total_cost, e)),
+            None => None,
+        }
     }
 }
