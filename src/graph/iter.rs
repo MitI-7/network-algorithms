a0b46991330f0a70c9750f329de166045b932d2f
MitI_7 <miti_7@outlook.com>
2026-01-02 10:03:50 +0900

    fix

---
+++ src/graph/iter.rs
@@ -0,0 +1,17 @@
+use crate::graph::ids::ArcId;
+
+pub(crate) struct ArcIdRange {
+    pub(crate) cur: usize,
+    pub(crate) end: usize, }
+
+impl Iterator for ArcIdRange {
+    type Item = ArcId;
+    #[inline]
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.cur >= self.end { return None; }
+        let a = ArcId(self.cur);
+        self.cur += 1;
+        Some(a)
+    }
+}
+
