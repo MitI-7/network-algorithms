20f9d3aec03e5a095d5ef893c51c0763ccd08ec9
MitI_7 <miti_7@outlook.com>
2026-01-16 14:14:59 +0900

    fix

---
+++ src/prelude.rs
@@ -0,0 +1,7 @@
+pub use crate::graph::{bipartite_graph::BipartiteGraph, graph::Graph, ids};
+
+pub use crate::algorithms::maximum_flow::prelude as maximum_flow;
+pub use crate::algorithms::maximum_matching::prelude as matching;
+pub use crate::algorithms::minimum_cost_flow::prelude as minimum_cost_flow;
+pub use crate::algorithms::shortest_path::prelude as shortest_path;
+pub use crate::algorithms::branching::prelude as branching;
